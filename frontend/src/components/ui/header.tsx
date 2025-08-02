ba1027c7eff0e19ff8b74812083bf03637f90d85
Ilhomjon <futurewithme1221@gmail.com>
2025-08-02 22:57:58 +0500

    design: Added popover to user avatar

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,13 +1,26 @@
+import { useNavigate } from "react-router";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Button } from "./button";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className="w-full h-14 shadow-sm flex justify-between items-center px-4">
       <div>chair time</div>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      <Popover>
+        <PopoverTrigger>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] flex flex-col p-1 gap-2 mr-5">
+            <Button variant="outline">Profile</Button>
+            <Button variant="outline" className="border border-red-400" onClick={() => navigate("login")}>Log out</Button>
+        </PopoverContent>
+      </Popover>
     </header>
   );
 }
