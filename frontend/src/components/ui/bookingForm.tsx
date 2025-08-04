ecd9f671786c19b7615e88fa20a750930c818f30
Ilhomjon <futurewithme1221@gmail.com>
2025-08-04 15:42:57 +0500

    design: Created booking page

---
+++ frontend/src/components/ui/bookingForm.tsx
@@ -0,0 +1,226 @@
+import { useState, type ReactNode } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
+import { Input } from "./input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+import { Button } from "./button";
+import { ChevronDownIcon } from "lucide-react";
+import { Calendar } from "./calendar";
+import { Textarea } from "./textarea";
+import { format } from "date-fns";
+
+const userName = ({ formControl }: { formControl: any }) => {
+  return (
+    <FormField
+      name="username"
+      control={formControl}
+      render={({ field }) => (
+        <FormItem className="mb-4">
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+const phone = ({ formControl }: { formControl: any }) => (
+  <FormField
+    control={formControl}
+    name="phone"
+    render={({ field }) => (
+      <FormItem className="mb-4">
+        <FormLabel>Phone</FormLabel>
+        <FormControl>
+          <Input placeholder="shadcn" {...field} />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+const master = ({ formControl }: { formControl: any }) => (
+  <FormField
+    control={formControl}
+    name="master"
+    render={({ field }) => (
+      <FormItem className="mb-4">
+        <FormLabel>Master</FormLabel>
+        <Select onValueChange={field.onChange} defaultValue={field.value}>
+          <FormControl className="w-full">
+            <SelectTrigger>
+              <SelectValue placeholder="Select a verified email to display" />
+            </SelectTrigger>
+          </FormControl>
+          <SelectContent>
+            <SelectItem value="m@example.com">m@example.com</SelectItem>
+            <SelectItem value="m@google.com">m@google.com</SelectItem>
+            <SelectItem value="m@support.com">m@support.com</SelectItem>
+          </SelectContent>
+        </Select>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+const style = ({ formControl }: { formControl: any }) => (
+  <FormField
+    control={formControl}
+    name="style"
+    render={({ field }) => (
+      <FormItem className="mb-4">
+        <FormLabel>Style</FormLabel>
+        <Select onValueChange={field.onChange} defaultValue={field.value}>
+          <FormControl className="w-full">
+            <SelectTrigger>
+              <SelectValue placeholder="Select a verified email to display" />
+            </SelectTrigger>
+          </FormControl>
+          <SelectContent>
+            <SelectItem value="m@example.com">m@example.com</SelectItem>
+            <SelectItem value="m@google.com">m@google.com</SelectItem>
+            <SelectItem value="m@support.com">m@support.com</SelectItem>
+          </SelectContent>
+        </Select>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+const date = ({ formControl }: { formControl: any }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <FormField
+      control={formControl}
+      name="date"
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel>Date </FormLabel>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <FormControl className="w-[50%]">
+                <Button
+                  variant="outline"
+                  id="date"
+                  className="w-48 justify-between font-normal"
+                >
+                  {field.value ? (
+                    format(field.value, "dd.MM.yyyy")
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
+                  <ChevronDownIcon />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent
+              className="w-auto overflow-hidden p-0"
+              align="start"
+            >
+              <Calendar
+                mode="single"
+                selected={field.value}
+                className="w-full"
+                captionLayout="dropdown"
+                onSelect={(date) => {
+                  field.onChange(date);
+                  setOpen(false);
+                }}
+              />
+            </PopoverContent>
+          </Popover>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+const time = ({ formControl }: { formControl: any }) => (
+  <FormField
+    control={formControl}
+    name="time"
+    render={({ field }) => (
+      <FormItem className="w-full">
+        <FormLabel>Time</FormLabel>
+        <Select onValueChange={field.onChange} defaultValue={field.value}>
+          <FormControl className="w-full">
+            <SelectTrigger>
+              <SelectValue placeholder="Select" className="w-full" />
+            </SelectTrigger>
+          </FormControl>
+          <SelectContent>
+            <SelectItem value="m@example.com">m@example.com</SelectItem>
+            <SelectItem value="m@google.com">m@google.com</SelectItem>
+            <SelectItem value="m@support.com">m@support.com</SelectItem>
+          </SelectContent>
+        </Select>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+const comment = ({ formControl }: { formControl: any }) => (
+  <FormField
+    control={formControl}
+    name="description"
+    render={({ field }) => (
+      <FormItem className="mb-4">
+        <FormLabel>Comment</FormLabel>
+        <FormControl>
+          <Textarea
+            placeholder="comment..."
+            className="resize-none"
+            {...field}
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export function BookingForm({
+  children,
+  form,
+  handleSubmit,
+}: {
+  children: ReactNode;
+  form: any;
+  handleSubmit: () => void;
+}) {
+  return (
+    <Form {...form}>
+      <form onSubmit={handleSubmit}>{children}</form>
+    </Form>
+  );
+}
+
+BookingForm.UserName = userName;
+BookingForm.Phone = phone;
+BookingForm.Master = master;
+BookingForm.Style = style;
+BookingForm.Date = date;
+BookingForm.Time = time;
+BookingForm.Comment = comment;
--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,26 +1,73 @@
 import { Button } from "@/components/ui/button";
-import { BookUser, ChevronLeftIcon } from "lucide-react";
+import { BookUser } from "lucide-react";
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { BookingForm } from "@/components/ui/bookingForm";
 
+type Inputs = {
+  username: string;
+  phone: string;
+  master: string;
+  style: string;
+  date: Date;
+  time: string;
+  description: string;
+};
+
+const formSchema = z.object({
+  username: z.string().min(1, "Username is required"),
+  phone: z.string().min(8, "Phone number is required"),
+  master: z.string().min(1, "Username is required"),
+  style: z.string().min(1, "Username is required"),
+  date: z.date(),
+  time: z.string().min(1, "Username is required"),
+  description: z.string().min(1, "Username is required"),
+});
 
 export function Booking() {
-   const navigate = useNavigate();
-   const {id} = useParams();
-   
-   useEffect(() => {
-      if(id) {
-        // fetch booking data
-        console.log("fetching,,,,")
-      }
-   }, [id])
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const form = useForm<Inputs>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "Arslan",
+      phone: "1234567890",
+    },
+    mode: "onChange",
+  });
 
-    return (
-        <>
-         <Button size="icon" className="size-8 absolute bottom-5" onClick={() => navigate('/')}>
-            <ChevronLeftIcon />
-        </Button>
-        <h1 className="font-bold text-left mb-2 font-mono flex gap-1 items-center"><BookUser size={18}/>Booking</h1>
-        </>
-    )
+  useEffect(() => {
+    if (id) {
+      // fetch booking data
+      console.log("fetching,,,,");
+    }
+  }, [id]);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="w-full">
+      <h1 className="font-bold text-left mb-4 font-mono flex gap-1 items-center">
+        <BookUser size={18} /> Booking
+      </h1>
+
+      <BookingForm form={form} handleSubmit={form.handleSubmit(onSubmit)}>
+        <BookingForm.UserName formControl={form.control} />
+        <BookingForm.Phone formControl={form.control} />
+        <BookingForm.Master formControl={form.control} />
+        <BookingForm.Style formControl={form.control} />
+        <div className="flex gap-2 w-full items-center mb-4">
+          <BookingForm.Date formControl={form.control} />
+          <BookingForm.Time formControl={form.control} />
+        </div>
+        <BookingForm.Comment formControl={form.control} />
+        <Button type="submit">Submit</Button>
+      </BookingForm>
+    </div>
+  );
 }
